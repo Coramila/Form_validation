d3c460dd1a4421b252c6b8c2d696c05428f87d0e
Camila Carvalho <carvalho.camiilaa@gmail.com>
2022-12-22 08:06:03 -0300

    CEP validation

--- js/validacao.js
+++ js/validacao.js
@@ -32,7 +32,7 @@ const mensagensDeErro = {
   },
   senha: {
     valueMissing: "O campo senha não pode estar vazio.",
-    patterMismatch:
+    patternMismatch:
       "A senha deve conter entre 6 a 12 caracteres, deve conter pelo menos uma letra maiscula, um número e não deve conter símbolos.",
   },
   dataNascimento: {
@@ -43,11 +43,25 @@ const mensagensDeErro = {
     valueMissing: "O campo data não pode estar vazio.",
     customError: "O CPF digitado não é válido.",
   },
+  cep: {
+    valueMissing: "O campo CEP não pode estar vazio.",
+    patternMismatch: "O CEP digitado não é válido.",
+  },
+  logradouro: {
+    valueMissing: "O campo logradouro não pode estar vazio.",
+  },
+  cidade: {
+    valueMissing: "O campo cidade não pode estar vazio.",
+  },
+  estado: {
+    valueMissing: "O campo estado não pode estar vazio.",
+  },
 };
 
 const validadores = {
   dataNascimento: (input) => validaDataNascimento(input),
   cpf: (input) => validaCPF(input),
+  cep: (input) => recuperarCEP(input),
 };
 
 function mostraMensagemDeErro(tipoDeInput, input) {
@@ -143,4 +157,23 @@ function checaDigitoVerificador(cpf, multiplicador) {
 
 function confirmaDigito(soma) {
   return 11 - (soma % 11);
+}
+
+function recuperarCEP(input) {
+  const cep = input.value.replace(/\D/g, "");
+  const url = `https://viacep.com.br/ws/${cep}/json`;
+  const options = {
+    method: "GET",
+    mode: "cors",
+    headers: {
+      "content-type": "application/json;charset=utf-8",
+    },
+  };
+  if (!input.validity.patternMismatch && !input.validity.valueMissing) {
+    fetch(url, options)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }
 }
